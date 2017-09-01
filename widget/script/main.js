6aeee42baee8d7bda0e06f16f36dd1bd51531600
zhengqiangzi <zhengqiangzi123@163.com>
2017-09-01 13:13:34 +0800

    fixed

--- widget/script/main.js
+++ widget/script/main.js
@@ -78,20 +78,51 @@ var vm=new __WEBPACK_IMPORTED_MODULE_0_vue___default.a({
     el: '#main',
 	data:{	
 
+		delItem:null,
 
+		glist:(function(){
+			var a=[]
+
+			var _list=(function(){
+
+				for(var i=0;i<=100;i++){
+
+					a.push({id:i+1,name:"name"+i})
+				}
+
+			})()
+
+			return a;
+
+		})()
 
 	},
 	computed:{
 		list:function(){
-			var a=[]
-			var _list=(function(){
-				for(var i=0;i<=100;i++){
-					a.push({id:i+1,name:"name"+i})
-				}
-			})()
-			return a;
+
+
+
+			if(this.delItem) {
+
+				this.glist=this.glist.filter((item)=>{
+
+					return item.id!=this.delItem.id
+
+				})
+			}
+
+			
+			return this.glist;
+			
 		}
 
+	},
+	methods:{
+
+		clickHandler:function(item){
+
+			this.delItem=item;
+		}
 	}
 
 })
--- widget/src/main.js
+++ widget/src/main.js
@@ -5,20 +5,51 @@ var vm=new Vue({
     el: '#main',
 	data:{	
 
+		delItem:null,
 
+		glist:(function(){
+			var a=[]
+
+			var _list=(function(){
+
+				for(var i=0;i<=100;i++){
+
+					a.push({id:i+1,name:"name"+i})
+				}
+
+			})()
+
+			return a;
+
+		})()
 
 	},
 	computed:{
 		list:function(){
-			var a=[]
-			var _list=(function(){
-				for(var i=0;i<=100;i++){
-					a.push({id:i+1,name:"name"+i})
-				}
-			})()
-			return a;
+
+
+
+			if(this.delItem) {
+
+				this.glist=this.glist.filter((item)=>{
+
+					return item.id!=this.delItem.id
+
+				})
+			}
+
+			
+			return this.glist;
+			
 		}
 
+	},
+	methods:{
+
+		clickHandler:function(item){
+
+			this.delItem=item;
+		}
 	}
 
 })
