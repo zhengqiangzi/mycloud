2ff6342f3efe9f1d9e143b72b72a76f0d9d0b838
zhengqiangzi <zhengqiangzi123@163.com>
2017-09-01 16:02:36 +0800

    fixed

--- widget/src/main.js
+++ widget/src/main.js
@@ -50,57 +50,8 @@ var vm=new Vue({
 
 
 		this.$nextTick(()=>{
-				var dialogBox = api.require('dialogBox');
-				dialogBox.alert({
-				    texts: {
-				        title: '确认',
-				        content: '送你一个超级礼包，内有超值福利，先到先得，点击领取 >>',
-				        leftBtnTitle: '取消',
-				        rightBtnTitle: '确认'
-				    },
-				    styles: {
-				        bg: '#fff',
-				        w: 300,
-				        title: {
-				            marginT: 20,
-				            icon: 'widget://res/gou.png',
-				            iconSize: 40,
-				            titleSize: 14,
-				            titleColor: '#000'
-				        },
-				        content: {
-				            color: '#000',
-				            size: 14
-				        },
-				        left: {
-				            marginB: 7,
-				            marginL: 20,
-				            w: 130,
-				            h: 35,
-				            corner: 2,
-				            bg: '#e0e',
-				            size: 12
-				        },
-				        right: {
-				            marginB: 7,
-				            marginL: 10,
-				            w: 130,
-				            h: 35,
-				            corner: 2,
-				            bg: '#e0e',
-				            size: 12
-				        }
-				    }
-				}, function(ret) {
-				    if (ret.eventType == 'left') {
-				        var dialogBox = api.require('dialogBox');
-				        dialogBox.close({
-				            dialogName: 'alert'
-				        });
-				    }
-				});
 
-
+			alert(api.appId)
 
 		})
 	}
